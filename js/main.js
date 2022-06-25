8d35da8486e230e9a7144672d4ab1d74d4586e35
salj8009 <salj8009@gmail.com>
2022-06-25 00:08:47 -0500

    creado el elemento para el checkout

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const Mas = document.getElementById('mas');
 const Menos = document.getElementById('menos');
 const countProduct = document.getElementById('count-product');
 const checkoutontent = document.getElementById('checkout-content');
+const imgCart = document.getElementById('imgCart');
 
 
 let cantidadProductos = 0;
@@ -36,10 +37,54 @@ Menosproductos();
 const fragment = document.createDocumentFragment();
 
 addToCart.addEventListener('click' , () => {
-    const pprice = document.createElement('P');
-    pprice.textContent = cantidadProductos;
-    fragment.appendChild(pprice);
+    
+    createElementsCheckout();
+    
+    console.log('btn add to cart');
+});
+
+imgCart.addEventListener('click' , () => {
+    createElementsCheckout();
+});
+
+const createElementsCheckout = () => {
+    const divcontent = document.createElement('DIV');
+    const parrafo = document.createElement('P');
+    const hr = document.createElement('HR');
+    const ptext = document.createElement('P');
+
+    divcontent.classList.add('checkout-content');
+    parrafo.classList.add('p-cart');
+    hr.classList.add('hr-container-checkout');
+    ptext.classList.add('p-text');
+
+    parrafo.textContent = `Cart`;
+    ptext.textContent = `You cart is empty`;
+
+    fragment.appendChild(divcontent);
+    fragment.appendChild(parrafo);
+    fragment.appendChild(hr);
+    fragment.appendChild(ptext);
 
     checkoutontent.appendChild(fragment);
-    console.log('btn add to cart');
-})
+  
+}
+
+/**
+    const divcontent = document.createElement('DIV');
+    const parrafo = document.createElement('P');
+    const pprice = document.createElement('P');
+
+    divcontent.classList('checkout-content');
+    parrafo.classList.add('checkout-content');
+    pprice.classList.add('p-cart');
+
+    parrafo.textContent = `Cart`;
+    pprice.textContent = cantidadProductos;
+
+    fragment.appendChild(divcontent);
+    fragment.appendChild(parrafo);
+    fragment.appendChild(pprice);
+
+    checkoutontent.appendChild(fragment); 
+ */
